17adbe509779bee4c045fa4eec9c5791a391ae19
lpigas <igornixdn@gmail.com>
2022-03-25 22:02:54 +0200

    add work proxi at indexArray with found many Id in file, change css , change html, all work

--- proxy/proxy_praktik.js
+++ proxy/proxy_praktik.js
@@ -46,21 +46,66 @@ let positions = withHiddenProps({
 
 // hidden properties 
 
-
+const x = []
+let y = []
 const indexArray = new Proxy(Array,{
     construct(target,[args]){
         const index = {}
-        args.forEach(el => index[el.id] = el);
+        args.forEach(el => {
+            if (el.id == 12001)
+            x.push(el)
+            index[el.id] = el
+            return x
+        });
+        return new Proxy (new target(...args), {
+            get (arr, prop){
+                switch(prop){
+                    case 'findById': return id =>{ // прототип поиска по и
+                        return x
+                    }
+                    default: return arr[prop]
+                }
+            }
+        })
+    }
+})
+
+const nameArray = new Proxy(Array,{
+    construct(target,[args]){
+        const index = {}
+        args.forEach(el => index[el.name] = el);
 
         return new Proxy (new target(...args), {
             get (arr, prop){
 
                 switch(prop){
                     case 'push': return item =>{   //создание прототипа пуш для добавления строки 
-                        index[item.id] = item
+                        index[item.name] = item
                         arr[prop].call(arr, item)
                     }
-                    case 'findById': return id =>{ // прототип поиска по и
+                    case 'findByName': return id =>{ // прототип поиска по и
+                        return index[id]
+                    }
+                    default: return arr[prop]
+                }
+            }
+        })
+    }
+})
+const jobArray = new Proxy(Array,{
+    construct(target,[args]){
+        const index = {}
+        args.forEach(el => index[el.job] = el);
+
+        return new Proxy (new target(...args), {
+            get (arr, prop){
+
+                switch(prop){
+                    case 'push': return item =>{   //создание прототипа пуш для добавления строки 
+                        index[item.job] = item
+                        arr[prop].call(arr, item)
+                    }
+                    case 'findByJob': return id =>{ // прототип поиска по и
                         console.log('what a hell I doing')
                         return index[id]
                     }
@@ -80,7 +125,7 @@ const ageArray = new Proxy(Array,{
         return new Proxy (new target(...args), {
             get (arr, prop){
                 switch(prop){
-                    case 'findByAge': return age =>{ // прототип поиска по и
+                    case 'findByage': return age =>{ // прототип поиска по и
                         console.log('what a hell you doing')
                         return document.body.innerHTML = `Name - ${ages[age].name}, job - ${ages[age].job}, age - ${ages[age].age}`
                     }
@@ -94,10 +139,12 @@ const ageArray = new Proxy(Array,{
 
 
 
-const userData = new indexArray([
+let userData = new indexArray([
     {id: 12001, name: 'Elena', job: 'home maker', age: 25},
-    {id: 12011, name: 'Viktor', job: 'Tester', age: 29},
-    {id: 12021, name: 'Ihor', job: 'Killer', age: 30},
-    {id: 12031, name: 'Anna', job: 'Student', age: 19},
+    {id: 12041, name: 'Viktor', job: 'Tester', age: 29},
+    {id: 12001, name: 'Ihor', job: 'Killer', age: 25},
+    {id: 12006, name: 'Anna', job: 'Student', age: 19},
 ])
 const userDatas = new ageArray(userData)
+const userName = new nameArray(userData)
+const jobName = new jobArray(userData)
--- work__from_sort/make_find_in_table/find.js
+++ work__from_sort/make_find_in_table/find.js
@@ -1,9 +1,13 @@
-const btnSelect= document.querySelectorAll('option');
-const inputFind = document.querySelector('#inputFind');
+const btnSelect= document.querySelector('[name=user_profile_find]');
+const inputinput__find = document.querySelector('#inputFind');
 const btnFind = document.querySelector('#btnFind');
 const btnReset = document.querySelector('.reset');
 const url = 'find.json';
 const table = document.querySelector('tbody')
+let changeFormValue = '';
+let newUrl = [];
+let x = []
+
 
 function addPersondata() {
     fetch(url)
@@ -12,6 +16,7 @@ function addPersondata() {
   })
   .then(function(text) {
     for(let i = 0; i < text.length; i ++){
+      newUrl.push(text[i])
         let makeTr = document.createElement('tr');
         makeTr.classList.add('table__firstLine')
 
@@ -50,65 +55,126 @@ addPersondata(url)
 
 
 
+for (let i = 0; i < btnSelect.options.length; ++i) {
+  btnSelect.options[i].foo = function() {
+  }
+}
+btnSelect.onchange = function() {
+  changeFormValue = this.options[this.selectedIndex].text;
+};
 
 
 
-// const indexArray = new Proxy(Array,{
-//     construct(target,[args]){
-//         const index = {}
-//         args.forEach(el => index[el.id] = el);
+btnFind.addEventListener('click',() =>{
+  let userData;
+  if (changeFormValue === 'Id'){
+    userData = new indexArray(newUrl)
+    console.log(userData.findById(inputinput__find.value))
+    } else if(changeFormValue === 'Name'  ) {
+      userData = new nameArray(newUrl)
+      console.log(userData.findByName(inputinput__find.value))
+    } else if(changeFormValue === 'Job') {
+      userData = new jobArray(newUrl)
+      console.log(userData.findByJob(inputinput__find.value))
+    } else  {
+      userData = new ageArray(newUrl)
+      console.log(userData.findByAge(inputinput__find.value))
+    }
 
-//         return new Proxy (new target(...args), {
-//             get (arr, prop){
-
-//                 switch(prop){
-//                     case 'push': return item =>{   //создание прототипа пуш для добавления строки 
-//                         index[item.id] = item
-//                         arr[prop].call(arr, item)
-//                     }
-//                     case 'findById': return id =>{ // прототип поиска по и
-//                         console.log('what a hell I doing')
-//                         return index[id]
-//                     }
-//                     default: return arr[prop]
-//                 }
-//             }
-//         })
-//     }
-// })
-
-// //make find by age
-// const ageArray = new Proxy(Array,{
-//     construct(target,[args]){
-//         const ages = {}
-//         args.forEach(el => ages[el.age] = el);
-
-//         return new Proxy (new target(...args), {
-//             get (arr, prop){
-//                 switch(prop){
-//                     case 'findByAge': return age =>{ // прототип поиска по и
-//                         console.log('what a hell you doing')
-//                         return document.body.innerHTML = `Name - ${ages[age].name}, job - ${ages[age].job}, age - ${ages[age].age}`
-//                     }
-//                     default: return arr[prop]
-//                 }
-//             }
-//         })
-//     }
-// })
+})
 
 
+const indexArray = new Proxy(Array,{
+  construct(target,[args]){
+      x = []
+      const index = {}
+      args.forEach(el => {
+          if (el.id == inputinput__find.value)
+          x.push(el)
+          index[el.id] = el
+          return x
+      });
+      return new Proxy (new target(...args), {
+          get (arr, prop){
+              switch(prop){
+                  case 'findById': return id =>{ // прототип поиска по и
+                      return x
+                  }
+                  default: return arr[prop]
+              }
+          }
+      })
+  }
+})
+
+const nameArray = new Proxy(Array,{
+  construct(target,[args]){
+      const index = {}
+      args.forEach(el => index[el.name] = el);
+
+      return new Proxy (new target(...args), {
+          get (arr, prop){
+
+              switch(prop){
+                  case 'push': return item =>{   //создание прототипа пуш для добавления строки 
+                      index[item.name] = item
+                      arr[prop].call(arr, item)
+                  }
+                  case 'findByName': return id =>{ // прототип поиска по и
+                      return index[id]
+                  }
+                  default: return arr[prop]
+              }
+          }
+      })
+  }
+})
+const jobArray = new Proxy(Array,{
+  construct(target,[args]){
+      const index = {}
+      args.forEach(el => index[el.job] = el);
+
+      return new Proxy (new target(...args), {
+          get (arr, prop){
+
+              switch(prop){
+                  case 'push': return item =>{   //создание прототипа пуш для добавления строки 
+                      index[item.job] = item
+                      arr[prop].call(arr, item)
+                  }
+                  case 'findByJob': return id =>{ // прототип поиска по и
+
+                      return index[id]
+                  }
+                  default: return arr[prop]
+              }
+          }
+      })
+  }
+})
+
+const ageArray = new Proxy(Array,{
+  construct(target,[args]){
+      const index = {}
+      args.forEach(el => index[el.age] = el);
+
+      return new Proxy (new target(...args), {
+          get (arr, prop){
+
+              switch(prop){
+                  case 'push': return item =>{   //создание прототипа пуш для добавления строки 
+                      index[item.age] = item
+                      arr[prop].call(arr, item)
+                  }
+                  case 'findByAge': return id =>{ // прототип поиска по и
+
+                      return index[id]
+                  }
+                  default: return arr[prop]
+              }
+          }
+      })
+  }
+})
 
 
-// const userData = new indexArray([
-//     {id: 12001, name: 'Elena', job: 'home maker', age: 25},
-//     {id: 12011, name: 'Viktor', job: 'Tester', age: 29},
-//     {id: 12021, name: 'Ihor', job: 'Killer', age: 30},
-//     {id: 12031, name: 'Anna', job: 'Student', age: 19},
-// ])
-// const userDatas = new ageArray(userData)
-
-
-// btnFind.addEventListener('click',() =>{
-//     console.log(btnSelect)
-// })
